d831f590510ffdb0e542281e832db4ed9eb1684c
SunshineInMoon <451413025@qq.com>
2019-10-14 21:33:15 +0800

    Update voc0712.py

--- data/voc0712.py
+++ data/voc0712.py
@@ -27,14 +27,14 @@ VOC_CLASSES = (  # always index 0
 # note: if you used our download scripts, this should be right
 VOC_ROOT = osp.join(HOME, "data/VOCdevkit/")
 
-
+# target = self.target_transform(target, width, height)
 class VOCAnnotationTransform(object):
     """Transforms a VOC annotation into a Tensor of bbox coords and label index
     Initilized with a dictionary lookup of classnames to indexes
 
     Arguments:
         class_to_ind (dict, optional): dictionary lookup of classnames -> indexes
-            (default: alphabetic indexing of VOC's 20 classes)
+            (default: alphabetic indexing of VOC's 20 classes) 类别名->整数
         keep_difficult (bool, optional): keep difficult instances or not
             (default: False)
         height (int): height
@@ -43,8 +43,8 @@ class VOCAnnotationTransform(object):
 
     def __init__(self, class_to_ind=None, keep_difficult=False):
         self.class_to_ind = class_to_ind or dict(
-            zip(VOC_CLASSES, range(len(VOC_CLASSES))))
-        self.keep_difficult = keep_difficult
+            zip(VOC_CLASSES, range(len(VOC_CLASSES)))) #类别名->整数
+        self.keep_difficult = keep_difficult #默认False
 
     def __call__(self, target, width, height):
         """
@@ -52,35 +52,37 @@ class VOCAnnotationTransform(object):
             target (annotation) : the target annotation to be made usable
                 will be an ET.Element
         Returns:
-            a list containing lists of bounding boxes  [bbox coords, class name]
+            a list containing lists of bounding boxes  [bbox coords, class name] #【盒子坐标，类别名字】
         """
-        res = []
-        for obj in target.iter('object'):
-            difficult = int(obj.find('difficult').text) == 1
+        res = [] #一张图片上可能有多个目标
+        for obj in target.iter('object'): #遍历图片中的目标，一张图片上可能有多个目标
+            difficult = int(obj.find('difficult').text) == 1 #xml中difficult用0 or 1表示，将其转换成True or False
             if not self.keep_difficult and difficult:
                 continue
-            name = obj.find('name').text.lower().strip()
-            bbox = obj.find('bndbox')
+            name = obj.find('name').text.lower().strip() #获取name，转换成小写字母
+            bbox = obj.find('bndbox') #获取框的坐标
 
             pts = ['xmin', 'ymin', 'xmax', 'ymax']
             bndbox = []
             for i, pt in enumerate(pts):
                 cur_pt = int(bbox.find(pt).text) - 1
                 # scale height or width
-                cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height
-                bndbox.append(cur_pt)
+                cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height #对框的坐标归一化
+                bndbox.append(cur_pt) #归一化后的坐标加入bndbox
             label_idx = self.class_to_ind[name]
             bndbox.append(label_idx)
-            res += [bndbox]  # [xmin, ymin, xmax, ymax, label_ind]
+            res += [bndbox]  # [xmin, ymin, xmax, ymax, label_ind] 
             # img_id = target.find('filename').text[:-4]
 
         return res  # [[xmin, ymin, xmax, ymax, label_ind], ... ]
 
-
+# dataset = VOCDetection(root=args.dataset_root,
+#                               transform=SSDAugmentation(cfg['min_dim'],
+#                                                         MEANS))
 class VOCDetection(data.Dataset):
     """VOC Detection Dataset Object
 
-    input is image, target is annotation
+    input is image, target is annotation(注释，坐标)
 
     Arguments:
         root (string): filepath to VOCdevkit folder.
@@ -96,19 +98,19 @@ class VOCDetection(data.Dataset):
 
     def __init__(self, root,
                  image_sets=[('2007', 'trainval'), ('2012', 'trainval')],
-                 transform=None, target_transform=VOCAnnotationTransform(),
+                 transform=None, target_transform=VOCAnnotationTransform(),# 解析xml
                  dataset_name='VOC0712'):
-        self.root = root
+        self.root = root #VOCdevkit folder路径
         self.image_set = image_sets
         self.transform = transform
         self.target_transform = target_transform
         self.name = dataset_name
-        self._annopath = osp.join('%s', 'Annotations', '%s.xml')
-        self._imgpath = osp.join('%s', 'JPEGImages', '%s.jpg')
+        self._annopath = osp.join('%s', 'Annotations', '%s.xml') #标注路径
+        self._imgpath = osp.join('%s', 'JPEGImages', '%s.jpg') #原始图片路径
         self.ids = list()
         for (year, name) in image_sets:
-            rootpath = osp.join(self.root, 'VOC' + year)
-            for line in open(osp.join(rootpath, 'ImageSets', 'Main', name + '.txt')):
+            rootpath = osp.join(self.root, 'VOC' + year) # VOCdevkit/VOC2007
+            for line in open(osp.join(rootpath, 'ImageSets', 'Main', name + '.txt')): #VOCdevkit/VOC2007/ImageSets/Main/trainval.txt
                 self.ids.append((rootpath, line.strip()))
 
     def __getitem__(self, index):
@@ -127,13 +129,13 @@ class VOCDetection(data.Dataset):
         height, width, channels = img.shape
 
         if self.target_transform is not None:
-            target = self.target_transform(target, width, height)
+            target = self.target_transform(target, width, height) #解析对应xml文件，获取目标框的坐标
 
         if self.transform is not None:
-            target = np.array(target)
-            img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
+            target = np.array(target) #转换成数组，这样每一行就是一个目标的框坐标和其对应的label_id
+            img, boxes, labels = self.transform(img, target[:, :4], target[:, 4]) #对输入图像和框的坐标进行处理
             # to rgb
-            img = img[:, :, (2, 1, 0)]
+            img = img[:, :, (2, 1, 0)] #图像转换成RGB
             # img = img.transpose(2, 0, 1)
             target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
         return torch.from_numpy(img).permute(2, 0, 1), target, height, width
