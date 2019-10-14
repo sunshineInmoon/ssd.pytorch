af2356669d500996c7b30b50486546f9b27b0cbd
SunshineInMoon <451413025@qq.com>
2019-10-14 21:33:22 +0800

    Update ssd.py

--- ssd.py
+++ ssd.py
@@ -13,7 +13,7 @@ class SSD(nn.Module):
     added multibox conv layers.  Each multibox layer branches into
         1) conv2d for class conf scores
         2) conv2d for localization predictions
-        3) associated priorbox layer to produce default bounding
+        3) associated(相关) priorbox(先验盒子) layer to produce default bounding
            boxes specific to the layer's feature map size.
     See: https://arxiv.org/pdf/1512.02325.pdf for more details.
 
@@ -24,14 +24,15 @@ class SSD(nn.Module):
         extras: extra layers that feed to multibox loc and conf layers
         head: "multibox head" consists of loc and conf conv layers
     """
-
+    # ssd_net = build_ssd('train', cfg['min_dim'], cfg['num_classes'])
+    # phase='train'; size=cfg['min_dim']=300
     def __init__(self, phase, size, base, extras, head, num_classes):
         super(SSD, self).__init__()
         self.phase = phase
         self.num_classes = num_classes
-        self.cfg = (coco, voc)[num_classes == 21]
-        self.priorbox = PriorBox(self.cfg)
-        self.priors = Variable(self.priorbox.forward(), volatile=True)
+        self.cfg = (coco, voc)[num_classes == 21] #如果num_classes=21,则self.cfg = voc, 否则self.cfg = coco
+        self.priorbox = PriorBox(self.cfg) #准备先验框
+        self.priors = Variable(self.priorbox.forward(), volatile=True) #获取先验框坐标
         self.size = size
 
         # SSD network
@@ -66,7 +67,7 @@ class SSD(nn.Module):
                     2: localization layers, Shape: [batch,num_priors*4]
                     3: priorbox layers, Shape: [2,num_priors*4]
         """
-        sources = list()
+        sources = list() #保存输出Tensor，将来要输入loc，conf中计算
         loc = list()
         conf = list()
 
@@ -123,6 +124,9 @@ class SSD(nn.Module):
 
 # This function is derived from torchvision VGG make_layers()
 # https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+# '300': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
+#            512, 512, 512]
+# batch_norm 使用False
 def vgg(cfg, i, batch_norm=False):
     layers = []
     in_channels = i
@@ -145,7 +149,10 @@ def vgg(cfg, i, batch_norm=False):
                nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
     return layers
 
-
+# cfg: '300': [256, 'S', 512, 128, 'S', 256, 128, 256, 128, 256]
+# i = 1024
+# batch_norm = Fasle
+# 增加conv8_1到conv11_2
 def add_extras(cfg, i, batch_norm=False):
     # Extra layers added to VGG for feature scaling
     layers = []
@@ -157,26 +164,27 @@ def add_extras(cfg, i, batch_norm=False):
                 layers += [nn.Conv2d(in_channels, cfg[k + 1],
                            kernel_size=(1, 3)[flag], stride=2, padding=1)]
             else:
-                layers += [nn.Conv2d(in_channels, v, kernel_size=(1, 3)[flag])]
+                layers += [nn.Conv2d(in_channels, v, kernel_size=(1, 3)[flag])] #flag=True kernel_size=3,否则kernel_size=1
             flag = not flag
         in_channels = v
     return layers
 
-
+# '300': [4, 6, 6, 6, 4, 4],每个锚点的先验框数量
+# 'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]]
 def multibox(vgg, extra_layers, cfg, num_classes):
     loc_layers = []
     conf_layers = []
-    vgg_source = [21, -2]
+    vgg_source = [21, -2] #21 vgg中conv4_3,不是relu4_3, 同理-2是conv7
     for k, v in enumerate(vgg_source):
         loc_layers += [nn.Conv2d(vgg[v].out_channels,
-                                 cfg[k] * 4, kernel_size=3, padding=1)]
+                                 cfg[k] * 4, kernel_size=3, padding=1)] #坐标总数，先验框数*4 [16,24]
         conf_layers += [nn.Conv2d(vgg[v].out_channels,
-                        cfg[k] * num_classes, kernel_size=3, padding=1)]
-    for k, v in enumerate(extra_layers[1::2], 2):
+                        cfg[k] * num_classes, kernel_size=3, padding=1)] #预测分数个数，先验框数*21 [84,126]
+    for k, v in enumerate(extra_layers[1::2], 2): #选取conv8_2,conv9_2...  k从2开始计数
         loc_layers += [nn.Conv2d(v.out_channels, cfg[k]
-                                 * 4, kernel_size=3, padding=1)]
+                                 * 4, kernel_size=3, padding=1)] #[24,24,16,16]
         conf_layers += [nn.Conv2d(v.out_channels, cfg[k]
-                                  * num_classes, kernel_size=3, padding=1)]
+                                  * num_classes, kernel_size=3, padding=1)] #[126,126,84,84]
     return vgg, extra_layers, (loc_layers, conf_layers)
 
 
